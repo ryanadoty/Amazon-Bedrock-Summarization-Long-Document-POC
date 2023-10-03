d87fef0932f1149bb7319bd662a111a574c2a841
Ryan Doty <rdoty@amazon.com>
2023-10-03 16:45:08 -0400

    refactor and clean up

--- app.py
+++ app.py
@@ -1,50 +1,49 @@
 import streamlit as st
 from pathlib import Path
 import os
-from doc_summarizer import fileChunker
+from doc_summarizer import Chunk_and_Summarize
 import time
+from dotenv import load_dotenv
 
-st.markdown("<h1 style='text-align: center; color: red;'>Long Document Summarization</h1>", unsafe_allow_html=True)
+# load environment variables
+load_dotenv()
+# title of the streamlit app
+st.title(f""":rainbow[Long Document Summarization with Amazon Bedrock]""")
 
-
-def main():
-    if "query_text" not in st.session_state:
-        st.session_state["query_text"] = []
-
-    if "query" not in st.session_state:
-        st.session_state["query"] = []
-
-    if "past" not in st.session_state:
-        st.session_state["past"] = []
-
-    with st.container():
-        st.header('Single File Upload')
-        File = st.file_uploader('Upload a file', type=["pdf"], key="new")
-        user_input = st.session_state["new"]
-
-        if File is not None:
-            st.session_state.past.append(user_input)
-            save_folder = "/Users/rdoty/Desktop/new_coding_projects/Amazon-Bedrock-Summarization-Long-Document-POC"
-            save_path = Path(save_folder, File.name)
-            with open(save_path, mode='wb') as w:
-                w.write(File.getvalue())
-
-            if save_path.exists():
-                st.success(f'File {File.name} is successfully saved!')
-                full_path = save_path.absolute()
-                my_path = full_path.as_posix()
-                start = time.time()
-                st.write(fileChunker(my_path))
-                end = time.time()
-                print(end - start)
-                print("DELETE")
-                os.remove(save_path)
-
-
-def clear_session():
-    for key in st.session_state.keys():
-        del st.session_state[key]
-
-
-if __name__ == "__main__":
-    main()
+# default container that houses the document upload field
+with st.container():
+    # header that is shown on the web UI
+    st.header('Single File Upload')
+    # the file upload field, the specific ui element that allows you to upload the file
+    File = st.file_uploader('Upload a file', type=["pdf"], key="new")
+    # when a file is uploaded it saves the file to the directory, creates a path, and invokes the
+    # Chunk_and_Summarize Function
+    if File is not None:
+        # determine the path to temporarily save the PDF file that was uploaded
+        save_folder = os.getenv("save_folder")
+        # create a posix path of save_folder and the file name
+        save_path = Path(save_folder, File.name)
+        # write the uploaded PDF to the save_folder you specified
+        with open(save_path, mode='wb') as w:
+            w.write(File.getvalue())
+        # once the save path exists...
+        if save_path.exists():
+            # write a success message saying the file has been successfully saved
+            st.success(f'File {File.name} is successfully saved!')
+            # creates a timer to time the length of the summarization task and starts the timer
+            start = time.time()
+            # running the summarization task, and outputting the results to the front end
+            st.write(Chunk_and_Summarize(save_path))
+            # ending the timer
+            end = time.time()
+            # using the timer, we calculate the minutes and seconds it took to perform the summarization task
+            seconds = int(((end - start) % 60))
+            minutes = int((end - start) // 60)
+            # string to highlight the amount of time taken to complete the summarization task
+            total_time = f"""Time taken to generate a summary:
+            Minutes: {minutes} Seconds: {round(seconds, 2)}"""
+            # sidebar is created to display the total time taken to complete the summarization task
+            with st.sidebar:
+                st.header(total_time)
+            # removing the PDF that was temporarily saved to perform the summarization task
+            os.remove(save_path)
--- doc_summarizer.py
+++ doc_summarizer.py
@@ -1,61 +1,107 @@
 import boto3
 import json
-from dotenv import load_dotenv
 import os
-from langchain.llms.bedrock import Bedrock
+import botocore.config
+from dotenv import load_dotenv
 from langchain.text_splitter import CharacterTextSplitter, RecursiveCharacterTextSplitter
-from langchain.document_loaders import PyPDFLoader, PyPDFDirectoryLoader
-from langchain.chains.summarize import load_summarize_chain
-from langchain.prompts import PromptTemplate
-from langchain.text_splitter import SpacyTextSplitter
+import tiktoken
+from pypdf import PdfReader
+
+load_dotenv()
+
+boto3.setup_default_session(profile_name=os.getenv("profile_name"))
+config = botocore.config.Config(connect_timeout=120, read_timeout=120)
+bedrock = boto3.client('bedrock-runtime', 'us-east-1', endpoint_url='https://bedrock.us-east-1.amazonaws.com',
+                       config=config)
+
+def summarizer(prompt_data) -> str:
+    """
+    This function creates the summary of each individual chunk as well as the final summary.
+    :param prompt_data: This is the prompt along with the respective chunk of text, at the end it contains all summary chunks combined.
+    :return: A summary of the respective chunk of data passed in or the final summary that is a summary of all summary chunks.
+    """
+    # setting the key parameters to invoke Amazon Bedrock
+    body = json.dumps({"prompt": prompt_data,
+                       "max_tokens_to_sample": 8191,
+                       "temperature": 0,
+                       "top_k": 250,
+                       "top_p": 0.5,
+                       "stop_sequences": []
+                       })
+    # the specific Amazon Bedrock model you are using
+    modelId = 'anthropic.claude-v2'
+    # type of data that should be expected upon invocation
+    accept = 'application/json'
+    contentType = 'application/json'
+    # the invocation of bedrock, with all of the parameters you have configured
+    response = bedrock.invoke_model(body=body,
+                                    modelId=modelId,
+                                    accept=accept,
+                                    contentType=contentType)
+    # gathering the response from bedrock, and parsing to get specifically the answer
+    response_body = json.loads(response.get('body').read())
+    answer = response_body.get('completion')
+    # returning the final summary for that chunk of text
+    return answer
 
 
-def fileChunker(uploaded_file):
-    llm = Bedrock(
-        credentials_profile_name="bedrock",
-        model_id="anthropic.claude-v2",
-        endpoint_url="https://bedrock.us-east-1.amazonaws.com",
-        region_name="us-east-1",
-        verbose=True
+def num_tokens_from_string(string) -> int:
+    """Returns the number of tokens in a text string."""
+    encoding = tiktoken.get_encoding("cl100k_base")
+    num_tokens = len(encoding.encode(string))
+    return num_tokens
+
+
+def Chunk_and_Summarize(uploaded_file) -> str:
+    """
+    This function takes in the path to the file that was just uploaded through the streamlit app.
+    :param uploaded_file: This is a file path, that should point to the newly uploaded file that is temporarily stored
+    within the directory of this application.
+    :return: This returns the final summary of the PDF document that was initially passed in by the user through the
+    streamlit app.
+    """
+    # using PyPDF PdfReader to read in the PDF file as text
+    reader = PdfReader(uploaded_file)
+    # creating an empty string for us to append all the text extracted from the PDF
+    text = ""
+    # a simple for loop to iterate through all pages of the PDF we uploaded
+    for page in reader.pages:
+        # as we loop through each page, we extract the text from the page and append it to the "text" string
+        text += page.extract_text() + "\n"
+    # creating the text splitter, we are specifically using the the recursive text splitter from langchain:
+    # https://python.langchain.com/docs/modules/data_connection/document_transformers/text_splitters/recursive_text_splitter
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=100,
+        length_function=len,
+        add_start_index=True
     )
-    loader = PyPDFLoader(uploaded_file)
-    documents = loader.load()
+    # using the text splitter to split the entire string of text that contains all the text content of our PDF
+    texts = text_splitter.create_documents([text])
+    # Creating an empty summary string, as this is where we will append the summary of each chunk
+    summary = ""
+    # looping through each chunk of text we created, passing that into our prompt and generating a summary of that chunk
+    for index, chunk in enumerate(texts):
+        # gathering the text content of that specific chunk
+        chunk_content = chunk.page_content
+        # creating the prompt that will be passed into Bedrock with the text content of the chunk
+        prompt = f"""\n\nHuman: Provide a detailed summary for the chunk of text provided to you:
+        Text: {chunk_content}
+        \n\nAssistant:"""
+        # passing the prompt into the summarizer function to generate the summary of that chunk, and appending it to
+        # the summary string
+        summary += summarizer(prompt)
+        # printing out the number of tokens contained in each chunk to provide a status update
+        print(f"\n\nNumber of tokens for Chunk {index + 1} with the prompt: {num_tokens_from_string(prompt)} tokens")
+        print("-------------------------------------------------------------------------------------------------------")
+    # after we have generated the summaries of each chunk of text, and appended them to the single summary string,
+    # we pass it into the final summary prompt
+    final_summary_prompt = f"""\n\nHuman: You will be given a set of summaries from a document. Create a cohesive 
+    summary from the provided individual summaries. The summary should very detailed and at least 2 pages. 
+    Summaries: {summary}
+            \n\nAssistant:"""
+    # print the total number of tokens being passed into the final summarization prompt
+    print(f"Number of tokens for this Chunk with the final prompt: {num_tokens_from_string(final_summary_prompt)}")
+    # generating the final summary of all the summaries we have previously generated.
+    return summarizer(final_summary_prompt)
 
-    text_splitter = CharacterTextSplitter.from_tiktoken_encoder(
-        chunk_size=3000, chunk_overlap=100
-    )
-
-    doc = text_splitter.split_documents(documents)
-
-    # avg_doc_length = lambda documents: sum([len(doc.page_content) for doc in documents]) // len(documents)
-    # avg_char_count_pre = avg_doc_length(documents)
-    # avg_char_count_post = avg_doc_length(doc)
-    # print(f'Average length among {len(documents)} documents loaded is {avg_char_count_pre} characters.')
-    # print(f'After the split we have {len(doc)} documents more than the original {len(documents)}.')
-    # print(f'Average length among {len(doc)} documents (after split) is {avg_char_count_post} characters.')
-
-    map_custom_prompt = '''
-
-    Human: You are a business professional and you need to create a 500 word summary on the documents provided with no postamble:
-    Documents:`{text}`
-
-
-    Assistant:
-
-    '''
-
-    map_prompt_template = PromptTemplate(
-        input_variables=['text'],
-        template=map_custom_prompt
-    )
-
-    summary_chain = load_summarize_chain(llm=llm,
-                                         chain_type="map_reduce",
-                                         map_prompt=map_prompt_template,
-                                         verbose=True,
-                                         token_max=12000)
-    # summary_chain = load_summarize_chain(llm=llm, chain_type="refine", refine_prompt=map_prompt_template,
-    #                                      verbose=True)
-
-    output = summary_chain.run(doc)
-    return output
