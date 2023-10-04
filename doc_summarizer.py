47e045936e2893e2b655e195243ef7cf4be36061
Ryan Doty <rdoty@amazon.com>
2023-10-04 08:26:57 -0400

    some code comments

--- doc_summarizer.py
+++ doc_summarizer.py
@@ -7,8 +7,9 @@ from langchain.text_splitter import CharacterTextSplitter, RecursiveCharacterTex
 import tiktoken
 from pypdf import PdfReader
 
+# loading environment variables
 load_dotenv()
-
+# configure Bedrock client
 boto3.setup_default_session(profile_name=os.getenv("profile_name"))
 config = botocore.config.Config(connect_timeout=120, read_timeout=120)
 bedrock = boto3.client('bedrock-runtime', 'us-east-1', endpoint_url='https://bedrock.us-east-1.amazonaws.com',
